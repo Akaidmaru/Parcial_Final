cd49982e5c5c7720a19b9c403115cb0fd2a55fb1
lauramtirado <alskling9@gmail.com>
2024-12-03 23:18:33 -0500

    chore: Extra work done
    Yeeha2

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 
 import Footer from "./Components/Footer";
 import Navbar from "./Components/Navbar";
+import { Route } from "react-router-dom";
+
 
 
 function App() {
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -5,7 +5,11 @@ import React from 'react'
 const Navbar = () => {
 
   return (
-    <nav>
+    <nav><Routes>
+      <Route path="/" element={<Home/>}/>
+    <Route path="/favs" element={<Favs/>}/>
+    <Route path="/contact" element={<Contact/>}/>
+    </Routes>
       {/* Aqui deberan agregar los liks correspondientes a las rutas definidas */}
       {/* Deberan implementar ademas la logica para cambiar de Theme con el button */}
       <button>Change theme</button>
